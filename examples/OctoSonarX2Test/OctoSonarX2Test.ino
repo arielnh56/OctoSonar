f0c77c11ce1398fcc169d7c2890869ee86536cf6
Alastair Young <alastair@redhunter.com>
2018-01-12 11:25:21 -0800

    Updates to X2 test and trimount bracket
    Updates X2 test
    Updated trimount bracket - moved diagonal sensor back

---
+++ examples/OctoSonarX2Test/OctoSonarX2Test.ino
@@ -0,0 +1,73 @@
+/* OctoSonarTest
+ *  
+ *  This is a basic test for the OctoSonar V2+ functionality
+ *  outputs range in mm to all 8 sensorts on 1602 or 2004 LCD and optional 
+ *  serial text.
+ *  
+ *  Copyright (c) 2017 Alastair Young
+ */
+#include "OctoSonar.h"
+#include <LiquidCrystal_PCF8574.h>
+
+#define SONAR_ADDR 0x38
+#define SONAR_INT 2
+#define ACTIVE_SONARS 0x00FF
+#define I2C_LCD_ADDRESS 0x27      // default for the current flavor of cheap I2C backpack
+//#define LCD_2004                // uncomment for 2004 display with labelled values
+//#define OUT_SERIAL              // uncomment for serial output
+
+OctoSonar myOcto(SONAR_ADDR, SONAR_INT);
+LiquidCrystal_PCF8574 lcd(I2C_LCD_ADDRESS);
+
+void setup() {
+#ifdef OUT_SERIAL
+  Serial.begin(115200);
+#endif
+
+#ifdef LCD_2004
+  lcd.begin(20, 4);              // initialize the lcd
+#else
+  lcd.begin(16, 2);              // initialize the lcd
+#endif
+  lcd.setBacklight(255);         // turn on the backlight
+  lcd.clear();                   // go home
+  myOcto.begin(ACTIVE_SONARS);   // initialize bus, pins etc
+}
+
+uint32_t last_print = 0;
+uint32_t last_disp = 0;
+void loop() {
+  char buffer[20];
+  OctoSonar::doSonar();  // call every cycle, OctoSonar handles the spacing
+
+#ifdef OUT_SERIAL
+    if (last_print + 200 < millis()) {   // serial output every 200ms
+      last_print = millis();
+      for (uint8_t i = 0; i < 8; i++) {
+        Serial.print(myOcto.read(i)); Serial.print("   ");
+      }
+      Serial.println();
+    }
+#endif
+  if (last_disp + 200 < millis()) {  // lcd update every 200ms
+    last_disp = millis();
+#ifdef LCD_2004
+    sprintf(buffer, "0 %5d      %5d 7 ", myOcto.read(0), myOcto.read(7));
+    lcd.setCursor(0, 0); lcd.print(buffer);
+    sprintf(buffer, "1 %5d      %5d 6", myOcto.read(1), myOcto.read(6));
+    lcd.setCursor(0, 1); lcd.print(buffer);
+    sprintf(buffer, "2 %5d      %5d 5", myOcto.read(2), myOcto.read(5));
+    lcd.setCursor(0, 2); lcd.print(buffer);
+    sprintf(buffer, "3 %5d      %5d 4", myOcto.read(3), myOcto.read(4));
+    lcd.setCursor(0, 3); lcd.print(buffer);
+#else
+    sprintf(buffer, "%4d%4d%4d%4d", myOcto.read(0), myOcto.read(1), myOcto.read(2), myOcto.read(3));
+    lcd.setCursor(0, 0); lcd.print(buffer);
+    sprintf(buffer, "%4d%4d%4d%4d", myOcto.read(4), myOcto.read(5), myOcto.read(6), myOcto.read(7));
+    lcd.setCursor(0, 1); lcd.print(buffer);
+#endif
+  }
+}
+
+
+
---
+++ examples/OctoSonarX2Test/OctoSonarX2Test12.ino
@@ -0,0 +1,74 @@
+/* OctoSonarX2Test12
+ *  
+ *  This is a basic test for the OctoSonarX2 functionality
+ *  outputs range in mm of 12 sensors on trimounts 1602 to 2004 and 0802 LCD and optional 
+ *  serial text.
+ *  
+ *  Copyright (c) 2017 Alastair Young
+ */
+#include "OctoSonar.h"
+#include <LiquidCrystal_PCF8574.h>
+
+#define SONAR_ADDR 0x20
+#define SONAR_INT 2
+#define ACTIVE_SONARS 0x7777
+#define I2C_LCD_ADDRESS 0x27      // default for the current flavor of cheap I2C backpack
+#define LCD_2004                // uncomment for 2004 display with labelled values
+#define I2C_LCD2_ADDRESS 0x26      // default for the current flavor of cheap I2C backpack
+#define LCD2_0802                // uncomment for 2004 display with labelled values
+//#define OUT_SERIAL              // uncomment for serial output
+
+OctoSonarX2 myOcto(SONAR_ADDR, SONAR_INT);
+LiquidCrystal_PCF8574 lcd(I2C_LCD_ADDRESS);
+LiquidCrystal_PCF8574 lcd2(I2C_LCD2_ADDRESS);
+
+void setup() {
+#ifdef OUT_SERIAL
+  Serial.begin(115200);
+#endif
+
+
+  lcd.begin(20, 4);              // initialize the lcd
+  lcd.setBacklight(255);         // turn on the backlight
+  lcd2.begin(8, 2);              // initialize the lcd
+  lcd2.setBacklight(255);         // turn on the backlight
+  lcd.clear();                   // go home
+  lcd2.clear();                   // go home
+  myOcto.begin(ACTIVE_SONARS);   // initialize bus, pins etc
+}
+
+uint32_t last_print = 0;
+uint32_t last_disp = 0;
+void loop() {
+  char buffer[20];
+  OctoSonar::doSonar();  // call every cycle, OctoSonar handles the spacing
+
+#ifdef OUT_SERIAL
+    if (last_print + 200 < millis()) {   // serial output every 200ms
+      last_print = millis();
+      for (uint8_t i = 0; i < 16; i++) {
+        Serial.print(myOcto.read(i)); Serial.print("   ");
+      }
+      Serial.println();
+    }
+#endif
+  if (last_disp + 200 < millis()) {  // lcd update every 200ms
+    last_disp = millis();
+    sprintf(buffer, "1 %5d      %5d C ", myOcto.read(1), myOcto.read(0xC));
+    lcd.setCursor(0, 0); lcd.print(buffer);
+    sprintf(buffer, "0 %5d      %5d D", myOcto.read(0), myOcto.read(0xD));
+    lcd.setCursor(0, 1); lcd.print(buffer);
+    sprintf(buffer, "5 %5d      %5d 8", myOcto.read(5), myOcto.read(8));
+    lcd.setCursor(0, 2); lcd.print(buffer);
+    sprintf(buffer, "4 %5d      %5d 9", myOcto.read(4), myOcto.read(9));
+    lcd.setCursor(0, 3); lcd.print(buffer);
+
+    sprintf(buffer, "%4d%4d", myOcto.read(2), myOcto.read(0xE));
+    lcd2.setCursor(0, 0); lcd2.print(buffer);
+    sprintf(buffer, "%4d%4d", myOcto.read(6), myOcto.read(0xA));
+    lcd2.setCursor(0, 1); lcd2.print(buffer);
+  }
+}
+
+
+
